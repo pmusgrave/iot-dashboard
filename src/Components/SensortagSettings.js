bd7c8be4cfcae08a2592944938b6653196f3c89e
pmusgrave <ps.musgrave@gmail.com>
2020-01-09 19:02:21 -0500

    add y axis labels to chart and change scale

--- src/Components/SensortagSettings.js
+++ src/Components/SensortagSettings.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { LineChart, Line, XAxis, CartesianGrid, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 const request = require('browser-request');
 
 let data = [];
+data = [21,22,21.3,21.4,22.5,22.6,22.7,22.8,22.9,22.10];
 
 class SensortagSettings extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class SensortagSettings extends Component {
           throw error;
         console.log(body);
         data = JSON.parse(body);
+
         this.setState({
           data: data
         });
@@ -43,6 +45,8 @@ class SensortagSettings extends Component {
             margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
           >
               <XAxis dataKey="time" />
+              <YAxis yAxisId={0} type="number" domain={[21,28]}/>
+              <YAxis yAxisId={1} orientation="right" type="number" domain={[0,100]}/>
               <Tooltip />
               <CartesianGrid stroke="#f5f5f5" />
               <Line type="monotone" dataKey="temperature" stroke="#ff7300" yAxisId={0} />
